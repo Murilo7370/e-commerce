bb3642c9a36ac04a82d0833e5d7b21f8ac8c43dc
Davi Martins <davi15mmartins@gmail.com>
2026-07-21 19:27:59 -0300

    fix: corrige navegação e exibição do nome da loja no componente principal

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,9 +3,12 @@ import {Produto} from '../produto/produto'
 import {signal} from '@angular/core'
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { effect} from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto,PrecoFormatadoPipe],
+  imports: [Produto,PrecoFormatadoPipe,UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -19,6 +22,7 @@ export class ListaProdutos {
 //!funçao para exibir produtos selecionados pelo usuario no console
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
   //!funçao que adicionar produto usando metodo update()
   adicionarProduto(){
@@ -34,7 +38,32 @@ export class ListaProdutos {
     valorTotal = computed(() => 
     {return this.produtos().reduce((total,item) =>
       total + item.preco,0)});
-      
+     //!funçao para substituir a lista atual usando o metodo set()
+    substituirProdutos(){
+      this.produtos.set([
+        {nome:'Teclado', preco:50},
+        {nome:'Mouse', preco:15},
+        {nome:'Monitor', preco:500},
+        {nome:'Desktop', preco:1500},
+        {nome:'headest', preco:30},
+      ]);
+    }
+    //! metodo para monitorar alteraçoes em tempo real usando effect()
+    constructor(){
+      effect(() => {
+      console.log('Lista de Produtos Alterados', this.produtos())  ;
+      });
+      effect(() => {
+        console.log('Valor Total Atualizado', this.valorTotal());
+      });
+      effect(() => {
+        if (typeof document !== 'undefined'){
+          document.title =`(${this.totalProdutos()}) - Loja do Ferreira`;
+        }
+      });
+    }
+    //! Metodo para criar um estado de seleçao com signal string | null
+    produtoSelecionado = signal <string | null>(null);
     }
   
   
